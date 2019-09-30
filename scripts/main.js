4a478593cf54087791bd32200ede71efadc5b7bc
Logan11999 <32521796+Logan11999@users.noreply.github.com>
2019-09-30 12:41:29 -0500

    finished challenge

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,14 @@
-
-
+const convertbtn = async (event) => {
+    const c = parseInt(document.querySelector('#cTemp').value)
+    const s = 273.15
+    const sub = c - s
+    const ans = `In Kelvin is ${sub}.`
+    document.querySelector('#result').innerHTML = ans
+    }
 
 document.addEventListener('click', event => {
-    if (event.target && event.target.id === 'convert') {  }
+    if (event.target && event.target.id === 'convertBtn') {  
+    console.log("clicked")
+    convertbtn(event)
+    }
   })
